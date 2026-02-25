c824a0c0695b201afdfdd43e710943f082a36a16
Tema <artempanov7576@gmail.com>
2026-02-25 21:32:58 +0300

    data concat

--- Data/c_vector.py
+++ Data/c_vector.py
@@ -1,40 +1,68 @@
 import pandas as pd
 from pathlib import Path
 
+def save_df(out_path, df):
+    out_path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(out_path, index=False)
+    print("Saved:", out_path)
+
 project_root = Path(__file__).resolve().parents[1]
-data_path = project_root / "Data" / "processed" / "apd6_cVAE.csv"
 
-df = pd.read_csv(data_path)
-print(df.columns.tolist())
+data_path = project_root / "Data" / "processed" / "data.csv"
+anticancer_path = project_root / "Data" / "processed" / "anticancer.csv"
+antiparasitic_path = project_root / "Data" / "processed" / "antiparasitic.csv"
+viral_path = project_root / "Data" / "processed" / "viral.csv"
 
-# for i in range(10):
-#     print(df.iloc[i]["Activity"])
-#
-# for i in range(10):
-#     print(df.iloc[i]["Additional info"])
+# data
+data_df = pd.read_csv(data_path)
+anticancer_df = pd.read_csv(anticancer_path)
+antiparasitic_df = pd.read_csv(antiparasitic_path)
+viral_df = pd.read_csv(viral_path)
 
-text = (
-    df["Activity"].fillna("").astype(str) + " " +
-    df["Activity_seq"].fillna("").astype(str)
-)
+datasets_names = [anticancer_df, antiparasitic_df, viral_df]
 
-m_gram_pos = text.str.contains(r"gram\+", case=False, regex=True, na=False)
-m_gram_neg = text.str.contains(r"gram-",  case=False, regex=True, na=False)
+for dataset in datasets_names:
+    text = (
+            dataset["Activity"].fillna("").astype(str)
+    )
 
-df["is_anti_gram_positive"] = m_gram_pos.astype(int)
-df["is_anti_gram_negative"] = m_gram_neg.astype(int)
-df["is_antibacterial"] = (m_gram_pos | m_gram_neg).astype(int)
+    m_gram_pos = text.str.contains(r"gram\+", case=False, regex=True, na=False)
+    m_gram_neg = text.str.contains(r"gram-", case=False, regex=True, na=False)
 
-df["is_antifungal"] = text.str.contains(r"\bantifungal\b",     case=False, regex=True, na=False).astype(int)
-df["is_antiviral"] = text.str.contains(r"\bantiviral\b",      case=False, regex=True, na=False).astype(int)
-df["is_antiparasitic"] = text.str.contains(r"\bantiparasitic\b",  case=False, regex=True, na=False).astype(int)
-df["is_anticancer"] = text.str.contains(r"\banticancer\b",     case=False, regex=True, na=False).astype(int)
+    dataset["is_anti_gram_positive"] = m_gram_pos.astype(int)
+    dataset["is_anti_gram_negative"] = m_gram_neg.astype(int)
+    dataset["is_antibacterial"] = (m_gram_pos | m_gram_neg).astype(int)
+    dataset["is_antifungal"] = text.str.contains(r"\bantifungal\b",     case=False, regex=True, na=False).astype(int)
+    dataset["is_antiviral"] = text.str.contains(r"\bantiviral\b", case=False, regex=True, na=False).astype(int)
+    dataset["is_antiparasitic"] = text.str.contains(r"\bantiparasitic\b",  case=False, regex=True, na=False).astype(int)
+    dataset["is_anticancer"] = text.str.contains(r"\banticancer\b",     case=False, regex=True, na=False).astype(int)
 
-print(df.iloc[17])
-for i in range(10):
-    print(df.iloc[i]["Activity_seq"])
+master_df = pd.concat([data_df, anticancer_df, antiparasitic_df, viral_df], ignore_index=True)
+master_df = master_df.drop_duplicates(subset="Sequence").reset_index(drop=True)
+print(f"Size before cleaning: {master_df.shape}")
+print(master_df.head())
 
-out_path = project_root / "Data" / "processed" / "data.csv"
-out_path.parent.mkdir(parents=True, exist_ok=True)
-df.to_csv(out_path, index=False)
-print("Saved:", out_path)
+out_path = project_root / "Data" / "processed" / "master_dataset_before_cleaning.csv"
+save_df(out_path, master_df)
+
+# Cleaning
+id_col = 'APD ID'
+seq_col = 'Sequence'
+len_col = 'Length'
+condition_cols = [
+    'is_antibacterial', 'is_anti_gram_positive', 'is_anti_gram_negative',
+    'is_antifungal', 'is_antiviral', 'is_antiparasitic', 'is_anticancer'
+]
+
+existing_cols = [col for col in [id_col, seq_col, len_col] + condition_cols if col in master_df.columns]
+df = master_df[existing_cols].dropna(subset=[seq_col])
+
+for col in condition_cols:
+    if col in df.columns:
+        df[col] = df[col].astype(int)
+
+print(f"Size after cleaning: {df.shape}")
+print(df.head())
+
+out_path = project_root / "Data" / "processed" / "master_dataset.csv"
+save_df(out_path, df)
